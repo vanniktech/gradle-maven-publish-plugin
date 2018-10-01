d474b9efdebadd87ac5b5ad1a731800b3d31c992
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-10-01 11:43:05 +0200

    Cosmetic changes. (#26)

--- src/main/kotlin/com/vanniktech/maven/publish/MavenPublishPluginExtension.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/MavenPublishPluginExtension.kt
@@ -11,26 +11,28 @@ import org.gradle.api.Project
 open class MavenPublishPluginExtension(project: Project) {
 
   private val defaultTarget = MavenPublishTarget(
-    DEFAULT_TARGET,
-    project.findProperty("RELEASE_REPOSITORY_URL") as String? ?: System.getenv("RELEASE_REPOSITORY_URL") ?: "https://oss.sonatype.org/service/local/staging/deploy/maven2/",
-    project.findProperty("SNAPSHOT_REPOSITORY_URL") as String? ?: System.getenv("SNAPSHOT_REPOSITORY_URL") ?: "https://oss.sonatype.org/content/repositories/snapshots/",
-    project.findProperty("SONATYPE_NEXUS_USERNAME") as String? ?: System.getenv("SONATYPE_NEXUS_USERNAME"),
-    project.findProperty("SONATYPE_NEXUS_PASSWORD") as String? ?: System.getenv("SONATYPE_NEXUS_PASSWORD"))
+      DEFAULT_TARGET,
+      project.findProperty("RELEASE_REPOSITORY_URL") as String? ?: System.getenv("RELEASE_REPOSITORY_URL") ?: "https://oss.sonatype.org/service/local/staging/deploy/maven2/",
+      project.findProperty("SNAPSHOT_REPOSITORY_URL") as String? ?: System.getenv("SNAPSHOT_REPOSITORY_URL") ?: "https://oss.sonatype.org/content/repositories/snapshots/",
+      project.findProperty("SONATYPE_NEXUS_USERNAME") as String? ?: System.getenv("SONATYPE_NEXUS_USERNAME"),
+      project.findProperty("SONATYPE_NEXUS_PASSWORD") as String? ?: System.getenv("SONATYPE_NEXUS_PASSWORD")
+  )
 
   private val localTarget = MavenPublishTarget(
-    LOCAL_TARGET,
-    releaseRepositoryUrl = project.repositories.mavenLocal().url.toASCIIString(),
-    signing = false)
+      LOCAL_TARGET,
+      releaseRepositoryUrl = project.repositories.mavenLocal().url.toASCIIString(),
+      signing = false
+  )
 
   /**
    * Allows to add additional [MavenPublishTargets][MavenPublishTarget] to publish to multiple repositories.
    * @since 0.7.0
    */
   val targets: NamedDomainObjectContainer<MavenPublishTarget> =
-    project.container(MavenPublishTarget::class.java) { MavenPublishTarget(it) }.apply {
-      add(defaultTarget)
-      add(localTarget)
-    }
+      project.container(MavenPublishTarget::class.java) { MavenPublishTarget(it) }.apply {
+        add(defaultTarget)
+        add(localTarget)
+      }
 
   /**
    * Allows to add additional [MavenPublishTargets][MavenPublishTarget] to publish to multiple repositories.
