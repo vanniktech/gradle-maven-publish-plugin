97729da42ff1c29dfb1c4624e035ece2c19c30ef
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-03-13 17:22:39 +0200

    Add first working test on Java project

---
+++ src/integrationTest/fixtures/passing_java_project/src/main/java/com/vanniktech/maven/publish/test/TestClass.java
@@ -0,0 +1,10 @@
+package com.vanniktech.maven.publish.test;
+
+/**
+ *
+ */
+public class TestClass {
+    public static void main(String[] args){
+        System.out.println( "Hello World!" );
+    }
+}
--- src/integrationTest/groovy/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.groovy
+++ src/integrationTest/groovy/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.groovy
@@ -1,11 +1,85 @@
 package com.vanniktech.maven.publish
 
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class MavenPublishPluginIntegrationTest {
+    @Rule
+    public TemporaryFolder testProjectDir = new TemporaryFolder()
+    File repoFolder
+    File buildFile
+    File gradlePropertiesFile
+
+    @Before
+    void setUp() throws Exception {
+        repoFolder = testProjectDir.newFolder("repo")
+
+        buildFile = testProjectDir.newFile('build.gradle')
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'com.vanniktech.maven.publish'
+            }
+            mavenPublish {
+                targets {
+                    installArchives {
+                        releaseRepositoryUrl = "file://${repoFolder.absolutePath}"
+                        signing = false
+                    }
+                }
+            }
+        """
+        gradlePropertiesFile = testProjectDir.newFile('gradle.properties')
+        gradlePropertiesFile << """
+            GROUP=com.example
+            VERSION_NAME=1.0.0
+            POM_ARTIFACT_ID=test-artifact
+        """
+
+        setupFixture("passing_java_project")
+    }
+
+    def setupFixture(String fixtureName) {
+        AntBuilder antBuilder = new AntBuilder()
+        antBuilder.copy(toDir: testProjectDir.root) {
+            fileset(dir: "src/integrationTest/fixtures/${fixtureName}")
+        }
+    }
 
     @Test
-    void test() {
-        assert true
+    void generatesArtifactsAndDocumentationOnJavaProject() {
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('javadocsJar', 'sourcesJar', 'installArchives', '--info')
+                .withPluginClasspath()
+                .build()
+
+        lsGeneratedFiles()
+
+        assert result.task(":installArchives").outcome == SUCCESS
+
+        def group = "com/example"
+        def artifactId = "test-artifact"
+        def version = "1.0.0"
+        def artifactFolder = "$repoFolder.absolutePath/$group/$artifactId/$version"
+        def artifactJar = "$artifactId-${version}.jar"
+        def javadocJar = "$artifactId-${version}-javadoc.jar"
+        def sourcesJar = "$artifactId-${version}-sources.jar"
+        assert new File("$artifactFolder/$artifactJar").exists()
+        assert new File("$artifactFolder/$javadocJar").exists()
+        assert new File("$artifactFolder/$sourcesJar").exists()
+    }
+
+    private void lsGeneratedFiles() {
+        def proc = "ls -R $repoFolder.absolutePath/com/example/test-artifact".execute()
+        def b = new StringBuffer()
+        proc.consumeProcessErrorStream(b)
+        println proc.text
+        println b.toString()
     }
 }
