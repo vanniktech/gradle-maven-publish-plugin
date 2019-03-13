8e919f594297bab4a487b425a9559c7938b025e6
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-03-13 13:37:56 +0200

    Add always passing test to prove it works

--- src/integrationTest/kotlin/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.kt
+++ src/integrationTest/kotlin/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.kt
@@ -1,2 +1,12 @@
 package com.vanniktech.maven.publish
 
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MavenPublishPluginIntegrationTest {
+
+  @Test
+  fun test() {
+    assertTrue(true)
+  }
+}
