80715fffefa0163fd45c12b3377cf43219ffc902
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-03-14 08:43:23 +0200

    Reorganize properly test on java project

--- src/integrationTest/groovy/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.groovy
+++ src/integrationTest/groovy/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.groovy
@@ -9,22 +9,34 @@ import org.junit.rules.TemporaryFolder
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class MavenPublishPluginIntegrationTest {
+    static final String TEST_GROUP = "com.example"
+    static final String TEST_VERSION_NAME = "1.0.0"
+    static final String TEST_POM_ARTIFACT_ID = "test-artifact"
+
     @Rule
     public TemporaryFolder testProjectDir = new TemporaryFolder()
     File repoFolder
     File buildFile
-    File gradlePropertiesFile
+
+    /**
+     * Copies test fixture into temp directory under test.
+     */
+    def setupFixture(String fixtureName) {
+        AntBuilder antBuilder = new AntBuilder()
+        antBuilder.copy(toDir: testProjectDir.root) {
+            fileset(dir: "src/integrationTest/fixtures/${fixtureName}")
+        }
+    }
 
     @Before
     void setUp() throws Exception {
         repoFolder = testProjectDir.newFolder("repo")
-
         buildFile = testProjectDir.newFile('build.gradle')
         buildFile << """
             plugins {
-                id 'java'
-                id 'com.vanniktech.maven.publish'
+              id "com.vanniktech.maven.publish"
             }
+
             mavenPublish {
                 targets {
                     installArchives {
@@ -34,52 +46,42 @@ class MavenPublishPluginIntegrationTest {
                 }
             }
         """
-        gradlePropertiesFile = testProjectDir.newFile('gradle.properties')
-        gradlePropertiesFile << """
-            GROUP=com.example
-            VERSION_NAME=1.0.0
-            POM_ARTIFACT_ID=test-artifact
+        testProjectDir.newFile('gradle.properties') << """
+            GROUP=$TEST_GROUP
+            VERSION_NAME=$TEST_VERSION_NAME
+            POM_ARTIFACT_ID=$TEST_POM_ARTIFACT_ID
         """
-
-        setupFixture("passing_java_project")
-    }
-
-    def setupFixture(String fixtureName) {
-        AntBuilder antBuilder = new AntBuilder()
-        antBuilder.copy(toDir: testProjectDir.root) {
-            fileset(dir: "src/integrationTest/fixtures/${fixtureName}")
-        }
     }
 
     @Test
     void generatesArtifactsAndDocumentationOnJavaProject() {
+        buildFile << """
+            apply plugin: "java"
+        """
+        setupFixture("passing_java_project")
+
         def result = GradleRunner.create()
                 .withProjectDir(testProjectDir.root)
                 .withArguments('javadocsJar', 'sourcesJar', 'installArchives', '--info')
                 .withPluginClasspath()
                 .build()
 
-        lsGeneratedFiles()
-
         assert result.task(":installArchives").outcome == SUCCESS
+        assertExpectedArtifactsGenerated()
+    }
 
+    private void assertExpectedArtifactsGenerated() {
         def group = "com/example"
         def artifactId = "test-artifact"
         def version = "1.0.0"
         def artifactFolder = "$repoFolder.absolutePath/$group/$artifactId/$version"
         def artifactJar = "$artifactId-${version}.jar"
+        def pomFile = "$artifactId-${version}.pom"
         def javadocJar = "$artifactId-${version}-javadoc.jar"
         def sourcesJar = "$artifactId-${version}-sources.jar"
         assert new File("$artifactFolder/$artifactJar").exists()
+        assert new File("$artifactFolder/$pomFile").exists()
         assert new File("$artifactFolder/$javadocJar").exists()
         assert new File("$artifactFolder/$sourcesJar").exists()
-    }
-
-    private void lsGeneratedFiles() {
-        def proc = "ls -R $repoFolder.absolutePath/com/example/test-artifact".execute()
-        def b = new StringBuffer()
-        proc.consumeProcessErrorStream(b)
-        println proc.text
-        println b.toString()
     }
 }
