f0a6a93847ae454aee0d6b889b565f6754dbaa0a
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-03-14 08:58:01 +0200

    Fix indendation of introduced classes

--- src/integrationTest/fixtures/passing_java_project/src/main/java/com/vanniktech/maven/publish/test/TestClass.java
+++ src/integrationTest/fixtures/passing_java_project/src/main/java/com/vanniktech/maven/publish/test/TestClass.java
@@ -4,12 +4,12 @@ package com.vanniktech.maven.publish.test;
  * Just a test class with Javadoc.
  */
 public class TestClass {
-    /**
-     * Main method which does something.
-     *
-     * @param args Command-line arguments passed to the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  /**
+   * Main method which does something.
+   *
+   * @param args Command-line arguments passed to the program.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
--- src/integrationTest/groovy/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.groovy
+++ src/integrationTest/groovy/com/vanniktech/maven/publish/MavenPublishPluginIntegrationTest.groovy
@@ -9,79 +9,78 @@ import org.junit.rules.TemporaryFolder
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class MavenPublishPluginIntegrationTest {
-    static final String TEST_GROUP = "com.example"
-    static final String TEST_VERSION_NAME = "1.0.0"
-    static final String TEST_POM_ARTIFACT_ID = "test-artifact"
+  static final String TEST_GROUP = "com.example"
+  static final String TEST_VERSION_NAME = "1.0.0"
+  static final String TEST_POM_ARTIFACT_ID = "test-artifact"
 
-    @Rule
-    public TemporaryFolder testProjectDir = new TemporaryFolder()
-    File repoFolder
-    File buildFile
+  @Rule
+  public TemporaryFolder testProjectDir = new TemporaryFolder()
+  File repoFolder
+  File buildFile
 
-    /**
-     * Copies test fixture into temp directory under test.
-     */
-    def setupFixture(String fixtureName) {
-        AntBuilder antBuilder = new AntBuilder()
-        antBuilder.copy(toDir: testProjectDir.root) {
-            fileset(dir: "src/integrationTest/fixtures/${fixtureName}")
+  /**
+   * Copies test fixture into temp directory under test.
+   */
+  def setupFixture(String fixtureName) {
+    AntBuilder antBuilder = new AntBuilder()
+    antBuilder.copy(toDir: testProjectDir.root) {
+      fileset(dir: "src/integrationTest/fixtures/${fixtureName}")
+    }
+  }
+
+  @Before
+  void setUp() throws Exception {
+    repoFolder = testProjectDir.newFolder("repo")
+    buildFile = testProjectDir.newFile('build.gradle')
+    buildFile << """
+      plugins {
+        id "com.vanniktech.maven.publish"
+      }
+      mavenPublish {
+        targets {
+          installArchives {
+            releaseRepositoryUrl = "file://${repoFolder.absolutePath}"
+            signing = false
+          }
         }
-    }
+      }
+    """
+    testProjectDir.newFile('gradle.properties') << """
+      GROUP=$TEST_GROUP
+      VERSION_NAME=$TEST_VERSION_NAME
+      POM_ARTIFACT_ID=$TEST_POM_ARTIFACT_ID
+    """
+  }
 
-    @Before
-    void setUp() throws Exception {
-        repoFolder = testProjectDir.newFolder("repo")
-        buildFile = testProjectDir.newFile('build.gradle')
-        buildFile << """
-            plugins {
-              id "com.vanniktech.maven.publish"
-            }
+  @Test
+  void generatesArtifactsAndDocumentationOnJavaProject() {
+    buildFile << """
+      apply plugin: "java"
+    """
+    setupFixture("passing_java_project")
 
-            mavenPublish {
-                targets {
-                    installArchives {
-                        releaseRepositoryUrl = "file://${repoFolder.absolutePath}"
-                        signing = false
-                    }
-                }
-            }
-        """
-        testProjectDir.newFile('gradle.properties') << """
-            GROUP=$TEST_GROUP
-            VERSION_NAME=$TEST_VERSION_NAME
-            POM_ARTIFACT_ID=$TEST_POM_ARTIFACT_ID
-        """
-    }
+    def result = GradleRunner.create()
+        .withProjectDir(testProjectDir.root)
+        .withArguments('javadocsJar', 'sourcesJar', 'installArchives', '--info')
+        .withPluginClasspath()
+        .build()
 
-    @Test
-    void generatesArtifactsAndDocumentationOnJavaProject() {
-        buildFile << """
-            apply plugin: "java"
-        """
-        setupFixture("passing_java_project")
+    assert result.task(":installArchives").outcome == SUCCESS
+    assertExpectedArtifactsGenerated()
+  }
 
-        def result = GradleRunner.create()
-                .withProjectDir(testProjectDir.root)
-                .withArguments('javadocsJar', 'sourcesJar', 'installArchives', '--info')
-                .withPluginClasspath()
-                .build()
-
-        assert result.task(":installArchives").outcome == SUCCESS
-        assertExpectedArtifactsGenerated()
-    }
-
-    private void assertExpectedArtifactsGenerated() {
-        def group = "com/example"
-        def artifactId = "test-artifact"
-        def version = "1.0.0"
-        def artifactFolder = "$repoFolder.absolutePath/$group/$artifactId/$version"
-        def artifactJar = "$artifactId-${version}.jar"
-        def pomFile = "$artifactId-${version}.pom"
-        def javadocJar = "$artifactId-${version}-javadoc.jar"
-        def sourcesJar = "$artifactId-${version}-sources.jar"
-        assert new File("$artifactFolder/$artifactJar").exists()
-        assert new File("$artifactFolder/$pomFile").exists()
-        assert new File("$artifactFolder/$javadocJar").exists()
-        assert new File("$artifactFolder/$sourcesJar").exists()
-    }
+  private void assertExpectedArtifactsGenerated() {
+    def group = "com/example"
+    def artifactId = "test-artifact"
+    def version = "1.0.0"
+    def artifactFolder = "$repoFolder.absolutePath/$group/$artifactId/$version"
+    def artifactJar = "$artifactId-${version}.jar"
+    def pomFile = "$artifactId-${version}.pom"
+    def javadocJar = "$artifactId-${version}-javadoc.jar"
+    def sourcesJar = "$artifactId-${version}-sources.jar"
+    assert new File("$artifactFolder/$artifactJar").exists()
+    assert new File("$artifactFolder/$pomFile").exists()
+    assert new File("$artifactFolder/$javadocJar").exists()
+    assert new File("$artifactFolder/$sourcesJar").exists()
+  }
 }
