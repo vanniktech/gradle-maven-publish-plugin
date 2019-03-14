9a7a4d47faf15ba8d1a4e7604268e12e1ed14ddf
vgaidarji <veaceslav.gaidarji@gmail.com>
2019-03-14 08:48:20 +0200

    Enhance test class javadocs

--- src/integrationTest/fixtures/passing_java_project/src/main/java/com/vanniktech/maven/publish/test/TestClass.java
+++ src/integrationTest/fixtures/passing_java_project/src/main/java/com/vanniktech/maven/publish/test/TestClass.java
@@ -1,10 +1,15 @@
 package com.vanniktech.maven.publish.test;
 
 /**
- *
+ * Just a test class with Javadoc.
  */
 public class TestClass {
-    public static void main(String[] args){
-        System.out.println( "Hello World!" );
+    /**
+     * Main method which does something.
+     *
+     * @param args Command-line arguments passed to the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
