474217bb190fe5f8129995853a0b40d76c369c64
Martin Bonnin <martin@mbonnin.net>
2019-05-28 17:46:25 +0200

    make most of the gradle properties optional again so that tests are happy

--- src/main/kotlin/com/vanniktech/maven/publish/MavenPublishPom.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/MavenPublishPom.kt
@@ -7,21 +7,21 @@ internal data class MavenPublishPom(
   val artifactId: String,
   val version: String,
 
-  val name: String,
-  val packaging: String,
+  val name: String?,
+  val packaging: String?,
   val description: String?,
-  val url: String,
+  val url: String?,
 
-  val scmUrl: String,
-  val scmConnection: String,
-  val scmDeveloperConnection: String,
+  val scmUrl: String?,
+  val scmConnection: String?,
+  val scmDeveloperConnection: String?,
 
-  val licenseName: String,
-  val licenseUrl: String,
-  val licenseDistribution: String,
+  val licenseName: String?,
+  val licenseUrl: String?,
+  val licenseDistribution: String?,
 
-  val developerId: String,
-  val developerName: String
+  val developerId: String?,
+  val developerName: String?
 ) {
 
   internal companion object {
@@ -30,18 +30,18 @@ internal data class MavenPublishPom(
         findMandatoryProperty(project, "GROUP"),
         findMandatoryProperty(project, "POM_ARTIFACT_ID"),
         findMandatoryProperty(project, "VERSION_NAME"),
-        findMandatoryProperty(project, "POM_NAME"),
-        findMandatoryProperty(project, "POM_PACKAGING"),
+        findOptionalProperty(project, "POM_NAME"),
+        findOptionalProperty(project, "POM_PACKAGING"),
         findOptionalProperty(project, "POM_DESCRIPTION"),
-        findMandatoryProperty(project, "POM_URL"),
-        findMandatoryProperty(project, "POM_SCM_URL"),
-        findMandatoryProperty(project, "POM_SCM_CONNECTION"),
-        findMandatoryProperty(project, "POM_SCM_DEV_CONNECTION"),
-        findMandatoryProperty(project, "POM_LICENCE_NAME"),
-        findMandatoryProperty(project, "POM_LICENCE_URL"),
-        findMandatoryProperty(project, "POM_LICENCE_DIST"),
-        findMandatoryProperty(project, "POM_DEVELOPER_ID"),
-        findMandatoryProperty(project, "POM_DEVELOPER_NAME")
+        findOptionalProperty(project, "POM_URL"),
+        findOptionalProperty(project, "POM_SCM_URL"),
+        findOptionalProperty(project, "POM_SCM_CONNECTION"),
+        findOptionalProperty(project, "POM_SCM_DEV_CONNECTION"),
+        findOptionalProperty(project, "POM_LICENCE_NAME"),
+        findOptionalProperty(project, "POM_LICENCE_URL"),
+        findOptionalProperty(project, "POM_LICENCE_DIST"),
+        findOptionalProperty(project, "POM_DEVELOPER_ID"),
+        findOptionalProperty(project, "POM_DEVELOPER_NAME")
     )
 
     private fun findMandatoryProperty(project: Project, propertyName: String): String {
