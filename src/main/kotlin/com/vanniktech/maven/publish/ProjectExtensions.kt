cb27ceb2253c3efdf02c962d0285b16349841878
Gabriel Ittner <gabriel@freeletics.com>
2020-02-02 15:18:26 +0100

    merge Utils into ProjectExtensions

--- src/main/kotlin/com/vanniktech/maven/publish/ProjectExtensions.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/ProjectExtensions.kt
@@ -1,13 +1,25 @@
 package com.vanniktech.maven.publish
 
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.plugins.signing.SigningExtension
+import java.util.concurrent.Callable
 
-fun Project.findMandatoryProperty(propertyName: String): String {
+internal fun Project.findMandatoryProperty(propertyName: String): String {
   val value = this.findOptionalProperty(propertyName)
   return requireNotNull(value) { "Please define \"$propertyName\" in your gradle.properties file" }
 }
 
-fun Project.findOptionalProperty(propertyName: String) = findProperty(propertyName)?.toString()
+internal fun Project.findOptionalProperty(propertyName: String) = findProperty(propertyName)?.toString()
 
-val Project.publishExtension: MavenPublishPluginExtension
+internal inline val Project.publishExtension: MavenPublishPluginExtension
   get() = project.extensions.getByType(MavenPublishPluginExtension::class.java)
+
+internal inline val Project.signing: SigningExtension
+  get() = extensions.getByType(SigningExtension::class.java)
+
+internal inline val Project.publishing: PublishingExtension
+  get() = extensions.getByType(PublishingExtension::class.java)
+
+internal inline val Project.isSigningRequired: Callable<Boolean>
+  get() = Callable<Boolean> { !project.version.toString().contains("SNAPSHOT") }
