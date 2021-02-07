57f039f975db16d878d75f481ef0b8f442b0e6e9
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-02-07 10:10:30 +0100

    simplify Nexus task setup and option to specify repository

--- src/main/kotlin/com/vanniktech/maven/publish/nexus/NexusOkHttpInterceptor.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/nexus/NexusOkHttpInterceptor.kt
@@ -4,7 +4,7 @@ import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class NexusOkHttpInterceptor(val username: String, val password: String) : Interceptor {
+internal class NexusOkHttpInterceptor(val username: String, val password: String) : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
     val requestBuilder = chain.request().newBuilder()
 
