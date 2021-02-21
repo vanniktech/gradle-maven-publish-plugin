6a38adadd0831a3388aa4a51b9fac291462b4bed
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-02-21 09:58:35 +0100

    update Gradle version check

--- src/main/kotlin/com/vanniktech/maven/publish/MavenPublishBasePlugin.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/MavenPublishBasePlugin.kt
@@ -9,8 +9,8 @@ open class MavenPublishBasePlugin : Plugin<Project> {
 
   override fun apply(project: Project) {
     val gradleVersion = VersionNumber.parse(project.gradle.gradleVersion)
-    if (gradleVersion < VersionNumber(MINIMUM_GRADLE_MAJOR, MINIMUM_GRADLE_MINOR, MINIMUM_GRADLE_MICRO, null)) {
-      throw IllegalArgumentException("You need Gradle version 6.6.0 or higher")
+    if (gradleVersion < MIN_GRADLE_VERSION) {
+      error("You need Gradle version 6.6.0 or higher")
     }
 
     project.plugins.apply(GradleMavenPublishPlugin::class.java)
@@ -19,8 +19,6 @@ open class MavenPublishBasePlugin : Plugin<Project> {
   }
 
   private companion object {
-    const val MINIMUM_GRADLE_MAJOR = 6
-    const val MINIMUM_GRADLE_MINOR = 6
-    const val MINIMUM_GRADLE_MICRO = 0
+    val MIN_GRADLE_VERSION = VersionNumber.parse("6.6.0")
   }
 }
