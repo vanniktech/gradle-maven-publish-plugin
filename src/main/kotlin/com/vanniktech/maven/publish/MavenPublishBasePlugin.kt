df3ad06beb357df131f710e08890ed457694703e
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-03-23 20:48:37 +0100

    Revert "always register closeAndReleaseRepository"
    This reverts commit aba6546eddc13e76cdc642dbe2883a26ceb93a7b.

--- src/main/kotlin/com/vanniktech/maven/publish/MavenPublishBasePlugin.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/MavenPublishBasePlugin.kt
@@ -1,9 +1,7 @@
 package com.vanniktech.maven.publish
 
-import com.vanniktech.maven.publish.nexus.CloseAndReleaseRepositoryTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.UnknownTaskException
 import org.gradle.util.VersionNumber
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin as GradleMavenPublishPlugin
 
@@ -18,20 +16,6 @@ open class MavenPublishBasePlugin : Plugin<Project> {
     project.plugins.apply(GradleMavenPublishPlugin::class.java)
 
     project.extensions.create("mavenPublishing", MavenPublishBaseExtension::class.java, project)
-
-    maybeRegisterCloseAndReleaseRepository(project)
-  }
-
-  private fun maybeRegisterCloseAndReleaseRepository(project: Project) {
-    @Suppress("SwallowedException")
-    try {
-      project.rootProject.tasks.named("closeAndReleaseRepository")
-    } catch (e: UnknownTaskException) {
-      project.rootProject.tasks.register("closeAndReleaseRepository", CloseAndReleaseRepositoryTask::class.java) {
-        it.description = "Closes and releases an artifacts repository in Nexus"
-        it.group = "release"
-      }
-    }
   }
 
   private companion object {
