dcfb6ba3991138698560d6bf4c314f72ed0a5e84
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-03-28 14:01:30 +0200

    add sonatypeHost to legacy extension, deprecate NexusOptions

--- src/main/kotlin/com/vanniktech/maven/publish/legacy/BaseSetup.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/legacy/BaseSetup.kt
@@ -1,13 +1,14 @@
 package com.vanniktech.maven.publish.legacy
 
-import com.vanniktech.maven.publish.SonatypeHost
 import com.vanniktech.maven.publish.baseExtension
 import com.vanniktech.maven.publish.findOptionalProperty
 import com.vanniktech.maven.publish.legacyExtension
 import org.gradle.api.Project
 
 internal fun Project.configureMavenCentral() {
-  baseExtension.publishToMavenCentral(SonatypeHost.DEFAULT)
+  afterEvaluate {
+    baseExtension.publishToMavenCentral(legacyExtension.sonatypeHost)
+  }
 }
 
 internal fun Project.configureSigning() {
