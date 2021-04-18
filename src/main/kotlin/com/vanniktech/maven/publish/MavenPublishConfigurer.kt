045297f151a238f9126ae01c9cfb6f92a84c8fc8
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-04-18 14:07:15 +0200

    fix detekt

--- src/main/kotlin/com/vanniktech/maven/publish/MavenPublishConfigurer.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/MavenPublishConfigurer.kt
@@ -96,7 +96,7 @@ internal class MavenPublishConfigurer(
     }
   }
 
-  class MissingVariantException(name: String) : RuntimeException(
+  internal class MissingVariantException(name: String) : RuntimeException(
     "Invalid MavenPublish Configuration. Unable to find variant to publish named $name." +
     " Try setting the 'androidVariantToPublish' property in the mavenPublish" +
     " extension object to something that matches the variant that ought to be published."
