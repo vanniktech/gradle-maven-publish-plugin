e1387c878d0ead50d5685c292a248250f4defc49
chenzhong.cz <chenzhong.cz@alibaba-inc.com>
2021-05-10 15:56:28 +0800

    provide an option to not publish the default repository

--- src/main/kotlin/com/vanniktech/maven/publish/legacy/BaseSetup.kt
+++ src/main/kotlin/com/vanniktech/maven/publish/legacy/BaseSetup.kt
@@ -19,7 +19,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 
 internal fun Project.configureMavenCentral() {
   afterEvaluate {
-    baseExtension.publishToMavenCentral(legacyExtension.sonatypeHost)
+    legacyExtension.sonatypeHost?.let { baseExtension.publishToMavenCentral(it) }
   }
 }
 
