a824079592fd0e1895aa0b293b798f593949fadb
Martin Bonnin <martin@mbonnin.net>
2021-08-07 13:06:52 +0200

    Make the Gradle project a multi-module build (#279)

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Version {
+  const val junit = "4.13.2"
+  const val assertj = "3.19.0"
+  const val agp = "3.6.0"
+  const val dokka = "0.9.18"
+  const val retrofit = "2.9.0"
+  const val moshi = "1.12.0"
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ pluginManagement {
     gradlePluginPortal()
   }
 }
+include(":plugin")
