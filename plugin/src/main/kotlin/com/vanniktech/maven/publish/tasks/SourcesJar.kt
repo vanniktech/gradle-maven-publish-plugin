b66a910281eaca663767e800bba7bfdf27d6ec48
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-06-02 22:35:15 +0200

    ktlint

--- plugin/src/main/kotlin/com/vanniktech/maven/publish/tasks/SourcesJar.kt
+++ plugin/src/main/kotlin/com/vanniktech/maven/publish/tasks/SourcesJar.kt
@@ -1,6 +1,5 @@
 package com.vanniktech.maven.publish.tasks
 
-import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.TaskProvider
