0a3d836dea8775775828812b7ae73fa2893aa487
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-06-03 11:10:40 +0200

    publish the plugin with the latest version of itself (#353)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
   implementation(kotlin("gradle-plugin"))
   implementation("com.github.ben-manes:gradle-versions-plugin:0.38.0")
   implementation("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
-  implementation("com.vanniktech:gradle-maven-publish-plugin:0.16.0-rc1")
+  implementation("com.vanniktech:gradle-maven-publish-plugin:0.20.0")
 }
 
 repositories {
