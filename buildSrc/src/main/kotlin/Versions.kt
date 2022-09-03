2b79118864d8cebeda8a83fdeae40f7582952adb
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-09-03 20:27:50 +0200

    update OkHttp to 4.10

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,6 +3,7 @@ object Version {
   const val assertj = "3.19.0"
   const val agp = "7.1.0"
   const val dokka = "1.6.10"
+  const val okhttp = "4.10.0"
   const val retrofit = "2.9.0"
   const val moshi = "1.13.0"
 }
