7b61e830c085adbf8061f82b3c669e010378db8f
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-09-03 19:42:21 +0200

    add methods for sonatype urls to SonatypeHost (#395)
    * add methods for sonatype urls to SonatypeHost
    
    * add import

--- plugin/src/main/kotlin/com/vanniktech/maven/publish/SonatypeHost.kt
+++ plugin/src/main/kotlin/com/vanniktech/maven/publish/SonatypeHost.kt
@@ -1,5 +1,7 @@
 package com.vanniktech.maven.publish
 
+import org.gradle.api.provider.Provider
+
 /**
  * Describes the various hosts for Sonatype OSSRH. Depending on when a user signed up with Sonatype
  * they have to use different hosts.
@@ -10,5 +12,25 @@ enum class SonatypeHost(
   internal val rootUrl: String
 ) {
   DEFAULT("https://oss.sonatype.org"),
-  S01("https://s01.oss.sonatype.org"),
+  S01("https://s01.oss.sonatype.org");
+
+  internal fun apiBaseUrl(): String {
+    return "$rootUrl/service/local/"
+  }
+
+  internal fun publishingUrl(snapshot: Boolean, stagingRepositoryId: Provider<String>): String {
+    return if (snapshot) {
+      if (stagingRepositoryId.isPresent) {
+        throw IllegalArgumentException("Staging repositories are not supported for SNAPSHOT versions.")
+      }
+      "$rootUrl/content/repositories/snapshots/"
+    } else {
+      val id = stagingRepositoryId.orNull
+      if (id != null) {
+        "$rootUrl/service/local/staging/deployByRepositoryId/$id/"
+      } else {
+        "$rootUrl/service/local/staging/deploy/maven2/"
+      }
+    }
+  }
 }
