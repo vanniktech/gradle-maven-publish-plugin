243b4b880f1d82538874d96b062565fe41d21dae
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-09-03 17:43:21 +0200

    change timout

--- nexus/src/main/kotlin/com/vanniktech/maven/publish/nexus/Nexus.kt
+++ nexus/src/main/kotlin/com/vanniktech/maven/publish/nexus/Nexus.kt
@@ -16,9 +16,9 @@ internal class Nexus(
   private val service by lazy {
     val okHttpClient = OkHttpClient.Builder()
       .addInterceptor(NexusOkHttpInterceptor(username, password))
-      .connectTimeout(30, TimeUnit.SECONDS)
-      .readTimeout(30, TimeUnit.SECONDS)
-      .writeTimeout(30, TimeUnit.SECONDS)
+      .connectTimeout(60, TimeUnit.SECONDS)
+      .readTimeout(60, TimeUnit.SECONDS)
+      .writeTimeout(60, TimeUnit.SECONDS)
       .build()
     val retrofit = Retrofit.Builder()
       .addConverterFactory(MoshiConverterFactory.create())
