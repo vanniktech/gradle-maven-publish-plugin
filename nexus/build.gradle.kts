651ea5b67a0368175554576cfaf37b9a384eaff5
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-09-04 11:59:12 +0200

    update OkHttp to 4.10 (#400)

--- nexus/build.gradle.kts
+++ nexus/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
   add("kapt", "com.squareup.moshi:moshi-kotlin-codegen:${Version.moshi}")
 
+  implementation("com.squareup.okhttp3:okhttp:${Version.okhttp}")
   implementation("com.squareup.moshi:moshi:${Version.moshi}")
   implementation("com.squareup.retrofit2:retrofit:${Version.retrofit}")
   implementation("com.squareup.retrofit2:converter-moshi:${Version.retrofit}")
