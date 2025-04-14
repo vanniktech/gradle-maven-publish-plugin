065f0d6a8176a701350db41d01aecb6ac9ca73af
Goooler <wangzongler@gmail.com>
2025-04-14 16:53:37 +0800

    Don't apply `buildconfig` to the root

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.ir.backend.js.compile
-
 plugins { `kotlin-dsl` }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
   alias(libs.plugins.kotlin) apply false
   alias(libs.plugins.mavenPublish) apply false
   alias(libs.plugins.ktlint) apply false
+  alias(libs.plugins.buildconfig) apply false
 }
