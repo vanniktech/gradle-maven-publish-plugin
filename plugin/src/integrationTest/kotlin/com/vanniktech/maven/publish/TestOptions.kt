949b73c0b9b758600c192df55d3aea5d8f0eddd0
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-04-21 18:43:33 +0200

    automate test version updates

--- plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/TestOptions.kt
+++ plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/TestOptions.kt
@@ -32,18 +32,11 @@ enum class AgpVersion(
     value = "8.0.0",
     minGradleVersion = GradleVersion.GRADLE_8_1,
   ),
-
-  // stable
-  AGP_8_9(
-    value = "8.9.0",
-    minGradleVersion = GradleVersion.GRADLE_8_10,
-  ),
-
-  // canary channel
-  AGP_8_10(
-    value = "8.10.0-alpha07",
-    minGradleVersion = GradleVersion.GRADLE_8_10,
-  ),
+  // latest versions of each type
+  AGP_STABLE(IntegrationTestBuildConfig.ANDROID_GRADLE_STABLE, GradleVersion.GRADLE_STABLE),
+  AGP_RC(IntegrationTestBuildConfig.ANDROID_GRADLE_RC, GradleVersion.GRADLE_STABLE),
+  AGP_BETA(IntegrationTestBuildConfig.ANDROID_GRADLE_BETA, GradleVersion.GRADLE_STABLE),
+  AGP_ALPHA(IntegrationTestBuildConfig.ANDROID_GRADLE_ALPHA, GradleVersion.GRADLE_STABLE),
 }
 
 enum class KotlinVersion(
@@ -53,12 +46,11 @@ enum class KotlinVersion(
 ) {
   // minimum supported
   KT_1_9_24("1.9.24"),
-
-  // stable
-  KT_2_1_10("2.1.10"),
-
-  // beta
-  KT_2_1_20("2.1.20-RC"),
+  // latest versions of each type
+  KOTLIN_STABLE(IntegrationTestBuildConfig.KOTLIN_STABLE),
+  KOTLIN_RC(IntegrationTestBuildConfig.KOTLIN_RC),
+  KOTLIN_BETA(IntegrationTestBuildConfig.KOTLIN_BETA),
+  KOTLIN_ALPHA(IntegrationTestBuildConfig.KOTLIN_ALPHA),
 }
 
 enum class GradleVersion(
@@ -70,28 +62,28 @@ enum class GradleVersion(
     value = "8.5",
     firstUnsupportedJdkVersion = JavaVersion.VERSION_22,
   ),
-
-  // stable
-  GRADLE_8_13(
-    value = "8.13",
-  ),
+  // latest versions of each type
+  GRADLE_STABLE(IntegrationTestBuildConfig.GRADLE_STABLE),
+  GRADLE_RC(IntegrationTestBuildConfig.GRADLE_RC),
+  GRADLE_BETA(IntegrationTestBuildConfig.GRADLE_BETA),
+  GRADLE_ALPHA(IntegrationTestBuildConfig.GRADLE_ALPHA),
   ;
 
   companion object {
-    // aliases for the skipped version to be able to reference the correct one in AgpVersion
+    // aliases for the skipped version to be able to reference the correct one in AgpVersion or conditions
     val GRADLE_8_1 = GRADLE_8_5
-    val GRADLE_8_9 = GRADLE_8_13
-    val GRADLE_8_10 = GRADLE_8_13
-    val GRADLE_8_12 = GRADLE_8_13
+    val GRADLE_8_12 = GRADLE_STABLE
   }
 }
 
 enum class GradlePluginPublish(val version: String) {
   // minimum supported
-  GRADLE_PLUGIN_PUBLISH_1_0("1.0.0"),
-
-  // stable
-  GRADLE_PLUGIN_PUBLISH_1_2("1.3.1"),
+  GRADLE_PLUGIN_PUBLISH_MIN("1.0.0"),
+  // latest versions of each type
+  GRADLE_PLUGIN_PUBLISH_STABLE(IntegrationTestBuildConfig.GRADLE_PUBLISH_STABLE),
+  GRADLE_PLUGIN_PUBLISH_RC(IntegrationTestBuildConfig.GRADLE_PUBLISH_RC),
+  GRADLE_PLUGIN_PUBLISH_BETA(IntegrationTestBuildConfig.GRADLE_PUBLISH_BETA),
+  GRADLE_PLUGIN_PUBLISH_ALPHA(IntegrationTestBuildConfig.GRADLE_PUBLISH_ALPHA),
 }
 
 fun GradleVersion.assumeSupportedJdkVersion() {
