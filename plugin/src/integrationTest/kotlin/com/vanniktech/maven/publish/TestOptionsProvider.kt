d71616aaab8d0c4e73b1e092b242737f42efb359
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-04-21 20:49:32 +0200

    remove kotlin 1.9 language features

--- plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/TestOptionsProvider.kt
+++ plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/TestOptionsProvider.kt
@@ -14,33 +14,33 @@ internal class TestOptionsConfigProvider : TestParameterValuesProvider() {
     if (quickTestProperty.isNotBlank()) {
       return ImmutableList.of(TestOptions.Config.BASE)
     }
-    return ImmutableList.copyOf(TestOptions.Config.entries)
+    return ImmutableList.copyOf(TestOptions.Config.values())
   }
 }
 
 internal class GradleVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(GradleVersion.entries.last())
+      return ImmutableList.of(GradleVersion.values().last())
     }
-    return ImmutableList.copyOf(GradleVersion.entries)
+    return ImmutableList.copyOf(GradleVersion.values())
   }
 }
 
 internal class AgpVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(AgpVersion.entries.last())
+      return ImmutableList.of(AgpVersion.values().last())
     }
-    return ImmutableList.copyOf(AgpVersion.entries)
+    return ImmutableList.copyOf(AgpVersion.values())
   }
 }
 
 internal class KotlinVersionProvider : TestParameterValuesProvider() {
   override fun provideValues(context: Context?): List<*> {
     if (quickTestProperty.isNotBlank()) {
-      return ImmutableList.of(KotlinVersion.entries.last())
+      return ImmutableList.of(KotlinVersion.values().last())
     }
-    return ImmutableList.copyOf(KotlinVersion.entries)
+    return ImmutableList.copyOf(KotlinVersion.values())
   }
 }
