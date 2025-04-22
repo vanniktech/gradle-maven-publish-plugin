ee9e49a1862cb6d51f9c264d92782d025f9c73b5
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-04-22 12:47:32 +0200

    fix staging profile selection (#929)
    * fix staging profile selection
    
    * ktlnt

--- nexus/build.gradle.kts
+++ nexus/build.gradle.kts
@@ -9,4 +9,12 @@ dependencies {
   implementation(libs.moshi)
   implementation(libs.retrofit)
   implementation(libs.retrofit.converter.moshi)
+
+  testImplementation(libs.junit.jupiter)
+  testImplementation(libs.truth)
+  testImplementation(libs.truth.java8)
+}
+
+tasks.withType(Test::class.java).configureEach {
+  useJUnitPlatform()
 }
