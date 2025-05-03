8010cc692fc440e764a87b84d557b03e85c8cbe8
Goooler <wangzongler@gmail.com>
2025-05-03 08:38:33 +0800

    No need to force stdlib and reflect lib in build logic

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -4,6 +4,4 @@ dependencies {
   implementation(libs.kotlin.plugin)
   implementation(libs.ktlint.plugin)
   implementation(libs.maven.publish.plugin)
-  // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
-  implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
--- build-logic/src/main/kotlin/shared.gradle.kts
+++ build-logic/src/main/kotlin/shared.gradle.kts
@@ -1,9 +1,6 @@
-import org.gradle.accessors.dm.LibrariesForLibs
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val libs = the<LibrariesForLibs>()
 
 plugins {
   id("java-library")
@@ -27,14 +24,5 @@ tasks.withType(KotlinCompile::class.java) {
   compilerOptions {
     jvmTarget.set(JvmTarget.JVM_11)
     languageVersion.set(KotlinVersion.KOTLIN_1_8)
-  }
-}
-
-configurations.all {
-  // Pin the kotlin version
-  resolutionStrategy {
-    force(libs.kotlin.stdlib)
-    force(libs.kotlin.stdlib.jdk8)
-    force(libs.kotlin.reflect)
   }
 }
