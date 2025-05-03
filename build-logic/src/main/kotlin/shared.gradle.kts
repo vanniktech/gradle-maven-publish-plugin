b32b4580b47edce0939cb734591215a545e284d4
Goooler <wangzongler@gmail.com>
2025-05-03 09:07:57 +0800

    Revert "No need to force stdlib and reflect lib in build logic"
    This reverts commit 8010cc692fc440e764a87b84d557b03e85c8cbe8.

--- build-logic/src/main/kotlin/shared.gradle.kts
+++ build-logic/src/main/kotlin/shared.gradle.kts
@@ -1,6 +1,9 @@
+import org.gradle.accessors.dm.LibrariesForLibs
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val libs = the<LibrariesForLibs>()
 
 plugins {
   id("java-library")
@@ -24,5 +27,14 @@ tasks.withType(KotlinCompile::class.java) {
   compilerOptions {
     jvmTarget.set(JvmTarget.JVM_11)
     languageVersion.set(KotlinVersion.KOTLIN_1_8)
+  }
+}
+
+configurations.all {
+  // Pin the kotlin version
+  resolutionStrategy {
+    force(libs.kotlin.stdlib)
+    force(libs.kotlin.stdlib.jdk8)
+    force(libs.kotlin.reflect)
   }
 }
