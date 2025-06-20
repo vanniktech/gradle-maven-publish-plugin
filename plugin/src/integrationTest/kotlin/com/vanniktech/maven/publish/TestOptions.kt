01bc88d5674d2edaea1ecba2e32a07f0d5d3c59e
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-06-20 23:43:41 +0200

    Update TestOptions.kt

--- plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/TestOptions.kt
+++ plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/TestOptions.kt
@@ -33,7 +33,7 @@ enum class AgpVersion(
   AGP_MIN(
     value = "8.0.0",
     minGradleVersion = GradleVersion.GRADLE_8_1,
-    firstUnsupportedGradleVersion = GradleVersion.GRADLE_ALPHA,
+    firstUnsupportedGradleVersion = GradleVersion.GRADLE_RC,
   ),
 
   // latest versions of each type
