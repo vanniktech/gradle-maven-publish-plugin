42acfd26be877e697e4485e1cb2a2363d8eabefd
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-06-21 14:42:42 +0200

    cleanup EndOfBuildAction
    - remove the ability to run releaseRepository with a provided id
    - remove leftovers of Drop supporting searching for repository to drop (was never supported for Central Portal)
    - rename Close and ReleaseAfterClose to Upload and Publish

--- plugin/src/main/kotlin/com/vanniktech/maven/publish/sonatype/ReleaseSonatypeRepositoryTask.kt
+++ plugin/src/main/kotlin/com/vanniktech/maven/publish/sonatype/ReleaseSonatypeRepositoryTask.kt
@@ -3,27 +3,18 @@ package com.vanniktech.maven.publish.sonatype
 import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.TaskProvider
-import org.gradle.api.tasks.options.Option
 
 internal abstract class ReleaseSonatypeRepositoryTask : DefaultTask() {
   @get:Internal
   abstract val buildService: Property<SonatypeRepositoryBuildService>
 
-  @Option(option = "repository", description = "Specify which staging repository to close and release.")
-  @Input
-  @Optional
-  var manualStagingRepositoryId: String? = null
-
   @TaskAction
-  fun closeAndReleaseRepository() {
-    val service = this.buildService.get()
-    service.shouldCloseAndReleaseRepository(manualStagingRepositoryId)
+  fun enableAutomaticPublishing() {
+    buildService.get().enableAutomaticPublishing()
   }
 
   companion object {
@@ -31,13 +22,11 @@ internal abstract class ReleaseSonatypeRepositoryTask : DefaultTask() {
 
     fun TaskContainer.registerReleaseRepository(
       buildService: Provider<SonatypeRepositoryBuildService>,
-      createRepository: TaskProvider<CreateSonatypeRepositoryTask>,
     ): TaskProvider<ReleaseSonatypeRepositoryTask> = register(NAME, ReleaseSonatypeRepositoryTask::class.java) {
       it.description = "Releases a staging repository on Sonatype OSS"
       it.group = "release"
       it.buildService.set(buildService)
       it.usesService(buildService)
-      it.mustRunAfter(createRepository)
     }
   }
 }
