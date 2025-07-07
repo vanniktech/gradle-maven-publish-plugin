d526d5c0ea5c0327151bfcdaef872d87fe731663
Goooler <wangzongler@gmail.com>
2025-07-07 11:01:08 +0800

    Migrate BCV to the Kotlin's builtin
    https://kotlinlang.org/docs/whatsnew22.html#binary-compatibility-validation-included-in-kotlin-gradle-plugin

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
   implementation(libs.kotlin.plugin)
   implementation(libs.kotlin.ksp)
   implementation(libs.maven.publish.plugin)
-  implementation(libs.bcv)
   // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
