7362779a394ac0b2b9060c6f9ad688aa4b659380
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-07-13 17:15:19 +0200

    make PrepareMavenCentralPublishingTask.localRepository internal

--- plugin/src/main/kotlin/com/vanniktech/maven/publish/central/PrepareMavenCentralPublishingTask.kt
+++ plugin/src/main/kotlin/com/vanniktech/maven/publish/central/PrepareMavenCentralPublishingTask.kt
@@ -6,7 +6,6 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskContainer
@@ -22,7 +21,7 @@ internal abstract class PrepareMavenCentralPublishingTask : DefaultTask() {
   @get:Input
   abstract val version: Property<String>
 
-  @get:InputFiles
+  @get:Internal
   abstract val localRepository: DirectoryProperty
 
   @get:Internal
