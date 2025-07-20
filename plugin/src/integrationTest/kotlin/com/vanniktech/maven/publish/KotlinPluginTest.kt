447d954137d6d8120d538df2dacd5ce1d24ff504
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-07-20 14:20:15 +0200

    add com.android.kotlin.multiplatform.library (#1096)
    * add com.android.kotlin.multiplatform.library
    
    * api
    
    * rename linux
    
    * Update CHANGELOG.md
    
    Co-authored-by: Zongle Wang <wangzongler@gmail.com>
    
    ---------
    
    Co-authored-by: Zongle Wang <wangzongler@gmail.com>

--- plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/KotlinPluginTest.kt
+++ plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/KotlinPluginTest.kt
@@ -1,5 +1,6 @@
 package com.vanniktech.maven.publish
 
+import com.google.common.truth.TruthJUnit.assume
 import com.google.testing.junit.testparameterinjector.junit5.TestParameter
 import com.google.testing.junit.testparameterinjector.junit5.TestParameterInjectorTest
 import com.vanniktech.maven.publish.ProjectResultSubject.Companion.assertThat
@@ -116,7 +117,7 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(jvmResult).javadocJar().exists()
     assertThat(jvmResult).javadocJar().isSigned()
 
-    val linuxResult = result.withArtifactIdSuffix("linux")
+    val linuxResult = result.withArtifactIdSuffix("linuxx64")
     assertThat(linuxResult).outcome().succeeded()
     assertThat(linuxResult).artifact("klib").exists()
     assertThat(linuxResult).artifact("klib").isSigned()
@@ -130,7 +131,7 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(linuxResult).module().isSigned()
     assertThat(linuxResult).sourcesJar().exists()
     assertThat(linuxResult).sourcesJar().isSigned()
-    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxMain")
+    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxX64Main")
     assertThat(linuxResult).javadocJar().exists()
     assertThat(linuxResult).javadocJar().isSigned()
 
@@ -195,7 +196,7 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(jvmResult).javadocJar().exists()
     assertThat(jvmResult).javadocJar().isSigned()
 
-    val linuxResult = result.withArtifactIdSuffix("linux")
+    val linuxResult = result.withArtifactIdSuffix("linuxx64")
     assertThat(linuxResult).outcome().succeeded()
     assertThat(linuxResult).artifact("klib").exists()
     assertThat(linuxResult).artifact("klib").isSigned()
@@ -209,7 +210,7 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(linuxResult).module().isSigned()
     assertThat(linuxResult).sourcesJar().exists()
     assertThat(linuxResult).sourcesJar().isSigned()
-    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxMain")
+    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxX64Main")
     assertThat(linuxResult).javadocJar().exists()
     assertThat(linuxResult).javadocJar().isSigned()
 
@@ -301,7 +302,7 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(jvmResult).javadocJar().exists()
     assertThat(jvmResult).javadocJar().isSigned()
 
-    val linuxResult = result.withArtifactIdSuffix("linux")
+    val linuxResult = result.withArtifactIdSuffix("linuxx64")
     assertThat(linuxResult).outcome().succeeded()
     assertThat(linuxResult).artifact("klib").exists()
     assertThat(linuxResult).artifact("klib").isSigned()
@@ -315,7 +316,7 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(linuxResult).module().isSigned()
     assertThat(linuxResult).sourcesJar().exists()
     assertThat(linuxResult).sourcesJar().isSigned()
-    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxMain")
+    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxX64Main")
     assertThat(linuxResult).javadocJar().exists()
     assertThat(linuxResult).javadocJar().isSigned()
 
@@ -371,5 +372,107 @@ class KotlinPluginTest : BasePluginTest() {
     assertThat(androidDebugResult).sourcesJar().containsSourceSetFiles("commonMain", "androidMain", "androidDebug")
     assertThat(androidDebugResult).javadocJar().exists()
     assertThat(androidDebugResult).javadocJar().isSigned()
+  }
+
+  @TestParameterInjectorTest
+  fun kotlinMultiplatformWithModernAndroidLibraryProject(
+    @TestParameter(valuesProvider = AgpVersionProvider::class) agpVersion: AgpVersion,
+    @TestParameter(valuesProvider = KotlinVersionProvider::class) kotlinVersion: KotlinVersion,
+  ) {
+    agpVersion.assumeSupportedJdkAndGradleVersion(gradleVersion)
+    kotlinVersion.assumeSupportedJdkAndGradleVersion(gradleVersion)
+
+    // requires AGP 8.11 and Kotlin 2.2 (maybe 2.1)
+    assume().that(agpVersion).isAtLeast(AgpVersion.AGP_STABLE)
+    assume().that(kotlinVersion).isAtLeast(KotlinVersion.KOTLIN_STABLE)
+
+    val project = kotlinMultiplatformWithModernAndroidLibraryProjectSpec(agpVersion, kotlinVersion)
+    val result = project.run(fixtures, testProjectDir, testOptions)
+
+    assertThat(result).outcome().succeeded()
+    assertThat(result).artifact("jar").exists()
+    assertThat(result).artifact("jar").isSigned()
+    assertThat(result).pom().exists()
+    assertThat(result).pom().isSigned()
+    assertThat(result).pom().matchesExpectedPom(
+      kotlinStdlibCommon(kotlinVersion).copy(scope = "runtime"),
+    )
+    assertThat(result).module().exists()
+    assertThat(result).module().isSigned()
+    assertThat(result).sourcesJar().exists()
+    assertThat(result).sourcesJar().isSigned()
+    assertThat(result).sourcesJar().containsSourceSetFiles("commonMain")
+    assertThat(result).javadocJar().exists()
+    assertThat(result).javadocJar().isSigned()
+
+    val jvmResult = result.withArtifactIdSuffix("jvm")
+    assertThat(jvmResult).outcome().succeeded()
+    assertThat(jvmResult).artifact("jar").exists()
+    assertThat(jvmResult).artifact("jar").isSigned()
+    assertThat(jvmResult).pom().exists()
+    assertThat(jvmResult).pom().isSigned()
+    assertThat(jvmResult).pom().matchesExpectedPom(
+      kotlinStdlibJdk(kotlinVersion),
+      kotlinStdlibCommon(kotlinVersion),
+    )
+    assertThat(jvmResult).module().exists()
+    assertThat(jvmResult).module().isSigned()
+    assertThat(jvmResult).sourcesJar().exists()
+    assertThat(jvmResult).sourcesJar().isSigned()
+    assertThat(jvmResult).sourcesJar().containsSourceSetFiles("commonMain", "jvmMain")
+    assertThat(jvmResult).javadocJar().exists()
+    assertThat(jvmResult).javadocJar().isSigned()
+
+    val linuxResult = result.withArtifactIdSuffix("linuxx64")
+    assertThat(linuxResult).outcome().succeeded()
+    assertThat(linuxResult).artifact("klib").exists()
+    assertThat(linuxResult).artifact("klib").isSigned()
+    assertThat(linuxResult).pom().exists()
+    assertThat(linuxResult).pom().isSigned()
+    assertThat(linuxResult).pom().matchesExpectedPom(
+      "klib",
+      kotlinStdlibCommon(kotlinVersion),
+    )
+    assertThat(linuxResult).module().exists()
+    assertThat(linuxResult).module().isSigned()
+    assertThat(linuxResult).sourcesJar().exists()
+    assertThat(linuxResult).sourcesJar().isSigned()
+    assertThat(linuxResult).sourcesJar().containsSourceSetFiles("commonMain", "linuxX64Main")
+    assertThat(linuxResult).javadocJar().exists()
+    assertThat(linuxResult).javadocJar().isSigned()
+
+    val nodejsResult = result.withArtifactIdSuffix("nodejs")
+    assertThat(nodejsResult).outcome().succeeded()
+    assertThat(nodejsResult).artifact("klib").exists()
+    assertThat(nodejsResult).artifact("klib").isSigned()
+    assertThat(nodejsResult).pom().exists()
+    assertThat(nodejsResult).pom().isSigned()
+    assertThat(nodejsResult).pom().matchesExpectedPom("klib", kotlinStdlibJs(kotlinVersion), kotlinDomApi(kotlinVersion))
+    assertThat(nodejsResult).module().exists()
+    assertThat(nodejsResult).module().isSigned()
+    assertThat(nodejsResult).sourcesJar().exists()
+    assertThat(nodejsResult).sourcesJar().isSigned()
+    assertThat(nodejsResult).sourcesJar().containsSourceSetFiles("commonMain", "nodeJsMain")
+    assertThat(nodejsResult).javadocJar().exists()
+    assertThat(nodejsResult).javadocJar().isSigned()
+
+    val androidReleaseResult = result.withArtifactIdSuffix("android")
+    assertThat(androidReleaseResult).outcome().succeeded()
+    assertThat(androidReleaseResult).artifact("aar").exists()
+    assertThat(androidReleaseResult).artifact("aar").isSigned()
+    assertThat(androidReleaseResult).pom().exists()
+    assertThat(androidReleaseResult).pom().isSigned()
+    assertThat(androidReleaseResult).pom().matchesExpectedPom(
+      "aar",
+      kotlinStdlibJdk(kotlinVersion),
+      kotlinStdlibCommon(kotlinVersion),
+    )
+    assertThat(androidReleaseResult).module().exists()
+    assertThat(androidReleaseResult).module().isSigned()
+    assertThat(androidReleaseResult).sourcesJar().exists()
+    assertThat(androidReleaseResult).sourcesJar().isSigned()
+    assertThat(androidReleaseResult).sourcesJar().containsSourceSetFiles("commonMain", "androidMain", "androidRelease")
+    assertThat(androidReleaseResult).javadocJar().exists()
+    assertThat(androidReleaseResult).javadocJar().isSigned()
   }
 }
