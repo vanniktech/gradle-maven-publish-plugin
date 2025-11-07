2e6622e9977974078e8b4f7c57e52493d2f286e0
Goooler <wangzongler@gmail.com>
2025-11-07 12:27:38 +0800

    Support deploying API html

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -2,6 +2,7 @@ plugins { `kotlin-dsl` }
 
 dependencies {
   implementation(libs.kotlin.plugin)
+  implementation(libs.dokka)
   implementation(libs.maven.publish.plugin)
   // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
