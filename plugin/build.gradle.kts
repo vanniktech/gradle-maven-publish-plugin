f62ac18fc3fec8aa336732d7d0029b130e6b0f96
Zongle Wang <wangzongler@gmail.com>
2025-11-14 05:55:38 +0800

    Reapply "Retry flaky tests on CI" (#1203)
    This reverts commit defd6402f4dc784948727d07c4774d09a6a0f3f0.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -157,6 +157,15 @@ val integrationTest by tasks.registering(Test::class) {
       logger.lifecycle("Running test: ${this.className} ${this.displayName}")
     },
   )
+
+  develocity {
+    testRetry {
+      if (providers.environmentVariable("CI").isPresent) {
+        maxRetries = 2
+        maxFailures = 10
+      }
+    }
+  }
 }
 
 tasks.test {
