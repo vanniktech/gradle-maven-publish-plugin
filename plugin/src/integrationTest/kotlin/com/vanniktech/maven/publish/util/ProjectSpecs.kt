c7b333c284ab66ed35fb090ad668d149a4702cbf
Goooler <wangzongler@gmail.com>
2025-11-20 15:10:59 +0800

    Revert "Use SPDX id for Apache-2.0"
    This reverts commit 11e1e0450836569ab05077bafb14c19b029eeea4.

--- plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/util/ProjectSpecs.kt
+++ plugin/src/integrationTest/kotlin/com/vanniktech/maven/publish/util/ProjectSpecs.kt
@@ -27,7 +27,7 @@ private val defaultProperties = mapOf(
   "POM_SCM_URL" to "https://github.com/vanniktech/gradle-maven-publish-plugin/",
   "POM_SCM_CONNECTION" to "scm:git:git://github.com/vanniktech/gradle-maven-publish-plugin.git",
   "POM_SCM_DEV_CONNECTION" to "scm:git:ssh://git@github.com/vanniktech/gradle-maven-publish-plugin.git",
-  "POM_LICENCE_NAME" to "Apache-2.0",
+  "POM_LICENCE_NAME" to "The Apache Software License, Version 2.0",
   "POM_LICENCE_URL" to "https://www.apache.org/licenses/LICENSE-2.0.txt",
   "POM_LICENCE_DIST" to "repo",
   "POM_DEVELOPER_ID" to "vanniktech",
